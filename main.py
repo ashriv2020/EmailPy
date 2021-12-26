66f478aa380fc4da57a7a734d09de6ac3f91109a
Abhishek <50742393+ashriv2020@users.noreply.github.com>
2021-12-26 08:13:21 +0000

    email with attachment from csv file

--- main.py
+++ main.py
@@ -18,15 +18,21 @@ yag = yagmail.SMTP(user=sender, password=os.getenv('Passgm'))
 df = pandas.read_csv('contacts.csv')
 #print(df)
 
+#modularize the code
+def send_email(name, toEmailAddress, amount, filepath):
+  #attach from csv file path, generate dynamic contents
+  contents_dynamic=[f"""Hi {name}, You have to pay {amount}.   
+  Please see attached file for details. Thanks.""",  filepath]
+  yag.send(to=toEmailAddress, subject= subject, contents = contents_dynamic)
+  # print(contents_dynamic)
+  print(f"Email Sent to {toEmailAddress}!!")
+
 #iterate through csv file rows
 for index, row in df.iterrows():
   toEmailAddress = row['email']
-  #attach from csv file path
-  contents_dynamic=[f"""Hi {row['name']},
-  You have billed amount {row['amount']}.   
-  Please see attached file for details. Thanks.""",  row['filepath']]
-  # print(contents_dynamic)
-  yag.send(to=toEmailAddress, subject= subject, contents = contents_dynamic)
-  print(f"Email Sent to {row['email']}!!")
+  name = row['name']
+  amount = row['amount']  
+  filepath= row['filepath']
+  send_email(name, toEmailAddress, amount, filepath)
 
 
